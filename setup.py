3b4e057710c520d589b1f235ad88d8e9db002c88
Sandeep Raju <me@sandeepraju.in>
2014-07-18 01:36:37 +0530

    Adds MANIFEST and setup configs

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014 Sandeep Raju. See LICENSE.txt for details.
+from setuptools import setup
+
+setup(
+    name='Subraminion',
+    version='0.0.1-dev',
+    url='https://github.com/sandeepraju/subraminion',
+    author='Sandeep Raju',
+    author_email='me@sandeepraju.in',
+    license=open('LICENSE.txt').read(),
+    description='',
+    long_description=open('README.md').read(),
+    packages=['subraminion'],
+    py_modules=['runner'],
+    install_requires=[
+    ],
+    entry_points="""
+    [console_scripts]
+    subraminion=runner:run
+    """
+    )
